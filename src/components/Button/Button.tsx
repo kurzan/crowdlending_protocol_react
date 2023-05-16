f370a673146cf5ec172d7f0e3a7949d3189959c0
Kurzov <kurzanbl4@gmail.com>
2023-05-16 22:34:36 +0300

    many bugs to fix

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -11,9 +11,10 @@ type TButtonProps = {
   isLoading?: boolean;
   style?: CSSProperties;
   type?: string;
+  submit?: any;
 };
 
-const Button: FC<TButtonProps> = ({type = 'web3', title, onClick, disabled, isLoading, style}) => {
+const Button: FC<TButtonProps> = ({submit, type = 'web3', title, onClick, disabled, isLoading, style}) => {
 
   const { connector: activeConnector, isConnected } = useAccount();
   const { connect, connectors, error, pendingConnector } = useConnect();
@@ -25,7 +26,7 @@ const Button: FC<TButtonProps> = ({type = 'web3', title, onClick, disabled, isLo
   }, [isConnected])
 
   return(
-      <button type="button" style={style} disabled={disabled} onClick={onClick} className={styles.button}>
+      <button type={submit} style={style} disabled={disabled} onClick={onClick} className={styles.button}>
         {isLoading && <Oval
           height={20}
           width={20}
--- src/pages/create-borrow/CreateBorrow.tsx
+++ src/pages/create-borrow/CreateBorrow.tsx
@@ -4,10 +4,51 @@ import styles from './CreateBorrow.module.css';
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
 import { useForm } from "../../hooks/useForms";
+import { useAccount, useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
+import { contract } from "../../services/web3config";
+import { ethers } from "ethers";
+
+const initialState = {
+  borrowingGoal: '0',
+  borrowingPeriod: '0',
+  interestRate: '0'
+}
 
 function CreateBorrow() {
 
-  const { values, handleChange, setValues } = useForm<any>({});
+  const { values, handleChange, setValues } = useForm<any>(initialState);
+  const { address, isConnected } = useAccount();
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalWaitIsOpen, setModalWaitIsOpen] = useState(false);
+  const [modalDoneIsOpen, setModalDoneIsOpen] = useState(false);
+
+  const { config, error, isLoading: prepareLoading } = usePrepareContractWrite({
+    address: contract.address,
+    abi: contract.abi,
+    functionName: 'createBorrow',
+    args: [ethers.utils.parseEther(values.borrowingGoal).toString(), values.borrowingPeriod, values.interestRate],
+    overrides: {
+      from: address,
+      value: ethers.utils.parseEther(values.borrowingGoal.toString())
+    }
+  });
+
+  const { data: investData, isLoading: isLoadingInvestData, isSuccess, write, reset } = useContractWrite({
+    ...config,
+    onSuccess(data) {
+      setModalIsOpen(false);
+      setModalWaitIsOpen(true);
+    },
+  });
+
+  const { data: dataWaitInvest, isError: errorWaitInvest, isLoading: loadingWaitInvest } = useWaitForTransaction({
+    hash: investData?.hash,
+    onSuccess(data) {
+      setModalWaitIsOpen(false);
+      setModalDoneIsOpen(true);
+    },
+  })
 
 
   useEffect(() => {
@@ -18,12 +59,12 @@ function CreateBorrow() {
     <LayoutPage>
       <p className={styles.heading}>Interact with a smart contract and create a borrow</p>
       <form className={styles.form} onSubmit={() => { }}>
-        
-        <Input label="Borrowing goal" name="borrowingGoal" value={values.borrowingGoal || ''} onChange={handleChange} type="number" min="10" />
-        <Input label="Period, days" name="borrowingPeriod" value={values.borrowingPeriod || ''} onChange={handleChange} type="number" />
-        <Input label="Interest rate" name="interestRate" value={values.interestRate || ''} onChange={handleChange} type="number" />
 
-        <Button type="submit" title="Create borrow" />
+        <Input label="Borrowing goal" placeholder={"min 0.1, max 1 tBNB"} name="borrowingGoal" value={values.borrowingGoal || ''} onChange={handleChange} type="number" min="0.1" max='1' />
+        <Input label="Period, days" placeholder={"min 7 days, max 90 days"} name="borrowingPeriod" value={values.borrowingPeriod || ''} onChange={handleChange} type="number" min="7" max='90'/>
+        <Input label="Interest rate" placeholder={"for ex. 9-15%"} name="interestRate" value={values.interestRate || ''} onChange={handleChange} type="number" />
+
+        <Button submit={"submit"} title="Create borrow" />
       </form>
     </LayoutPage>
   );
