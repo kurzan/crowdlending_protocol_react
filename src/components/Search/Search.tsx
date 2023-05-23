c5fb1f85c7e1a5ebbaf5f63ba91c64771b3876a1
Kurzov <kurzanbl4@gmail.com>
2023-05-23 16:15:25 +0300

    Fix search query browser and reset rearch value

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,8 @@
-import { FC, useContext, useEffect, useRef } from 'react';
+import { FC, useEffect, useState } from 'react';
 import search_img from '../../images/search.svg';
 import styles from './Search.module.css';
 import { useSearchParams } from 'react-router-dom';
-
+import { MdClose } from 'react-icons/md';
 
 type TSearch = {
   placeholder: string;
@@ -10,22 +10,42 @@ type TSearch = {
 };
 
 export const Search: FC<TSearch> = ({ placeholder, setSearch }) => {
-
   const [searchParams, setSearchParams] = useSearchParams();
+  const [searchValue, setSearchValue] = useState(searchParams.get("search") || "");
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let search = e.target.value;
-    if (search) {
-      setSearchParams({search});
-    } else {
-      setSearchParams({});
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearchValue = e.target.value;
+    setSearchValue(newSearchValue);
+
+    const search = searchParams.get("search");
+    if (!newSearchValue && search) {
+      const newParams = new URLSearchParams(searchParams);
+      newParams.delete("search");
+      setSearchParams(newParams);
+    } else if (newSearchValue !== search) {
+      const newParams = new URLSearchParams(searchParams);
+      newParams.set("search", newSearchValue);
+      setSearchParams(newParams);
     }
+  };
+
+  useEffect(() => {
+    setSearchValue(searchParams.get("search") || "");
+  }, [searchParams, setSearchValue]);
+
+  const handleReset = () => {
+    // Удаляем параметр search из параметров поиска
+    const newParams = new URLSearchParams(searchParams);
+    newParams.delete("search");
+    setSearchParams(newParams);
+    setSearchValue("");
   };
 
   return (
     <div className={styles.inputBox}>
       <img src={search_img} alt="search"/>
-      <input className={styles.input} placeholder={placeholder} onChange={onChange} value={searchParams.get('search') || ''} />
+      <input className={styles.input} placeholder={placeholder} onChange={handleChange} value={searchValue || ''} />
+      {searchValue.length > 0 && <MdClose size={30} color='#BCC5CD' onClick={handleReset}/>}
     </div>
-    )
-};
+  );
+}
