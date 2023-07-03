05d8f4ac6717ff41a416d24c3c09d36eddbdbecb
Kurzov <kurzanbl4@gmail.com>
2023-07-03 13:09:53 +0300

    change chain to siberium

--- src/services/web3config.ts
+++ src/services/web3config.ts
@@ -1,20 +1,36 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
-import { configureChains, createClient } from "wagmi";
-import { bscTestnet } from 'wagmi/chains';
+import { Chain, configureChains, createClient } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { contractAbi } from "./abi";
 
-// const CONTRACT_ADDRESS: `0x${string}` = "0xB5E15A65EEFB837A130aCbFcB8beB3C1fefaD092";
+const CONTRACT_ADDRESS: `0x${string}` = "0xD671AF87E6D8e1Da5F8aA0ad4fC0eBEE41b43eD2";
 
-const CONTRACT_ADDRESS: `0x${string}` = "0xB644E311E961f09695EE46e9568f21999907ffd0";
-
-
+export const siberiumTestnet = {
+  id: 111000,
+  name: 'SiberiumTestnet',
+  network: 'siberium-testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Siberium',
+    symbol: 'SIBR',
+  },
+  rpcUrls: {
+    public: { http: ['https://rpc.test.siberium.net'] },
+    default: { http: ['https://rpc.test.siberium.net'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'Siberium Testnet Epxlorer', url: 'https://explorer.test.siberium.net/' },
+    default: { name: 'Siberium Testnet Epxlorer', url: 'https://explorer.test.siberium.net/' },
+  },
+  contracts: {
+  },
+} as const satisfies Chain
 
 
 export const { chains, provider, webSocketProvider } = configureChains(
   [
-    bscTestnet,
+    siberiumTestnet,
   ],
   [publicProvider()]
 );
